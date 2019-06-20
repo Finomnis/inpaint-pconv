65412aeec0027d7765e738c52e1cc5a0d2fe1d26
Finomnis <martin.h.stumpf@gmail.com>
2019-06-20 22:57:20 +0200

    more work on dataloader, now automatically resizes to 512

--- DataLoader.py
+++ DataLoader.py
@@ -1,12 +1,14 @@
 from torch.utils.data import Dataset
+import torch
 from torchvision import transforms
 
 from PIL import Image
 
+import numpy as np
+
 import os
 import glob
 import random
-
 
 class MaskedImageDataset(Dataset):
 
@@ -14,10 +16,20 @@ class MaskedImageDataset(Dataset):
     stddev = [0.229, 0.224, 0.225]
 
     @staticmethod
-    def unnormalize(img):
-        pass
+    def to_img(x):
+        x.transpose_(0, 2)
+        x = x * torch.Tensor(MaskedImageDataset.stddev) + torch.Tensor(MaskedImageDataset.mean)
+
+        if x.size(2) == 3:
+            img = Image.fromarray(x.cpu().detach().numpy(), 'RGB')
+        else:
+            img = Image.fromarray(x.cpu().detach().numpy(), 'L')
+
+        return img
 
     def __init__(self, img_path, mask_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'masks', 'generated')):
+        super(MaskedImageDataset, self).__init__()
+
         self.imgs = glob.glob(img_path + '**/*.jpg', recursive=True)
         self.masks = glob.glob(mask_path + '**/*.png')
 
@@ -27,9 +39,15 @@ class MaskedImageDataset(Dataset):
 
     def __getitem__(self, index):
         img = Image.open(self.imgs[index]).convert('RGB')
-        mask = Image.open(random.choice(self.masks)).convert('L')
+
+        crop_size = min(img.size)
+        img = transforms.RandomCrop((crop_size, crop_size))(img)
+        if crop_size < 512:
+            img = transforms.Resize((512, 512))(img)
 
         img = self.img_transform(img)
+
+        mask = Image.open(random.choice(self.masks)).convert('RGB')
         mask = self.mask_transform(mask)
 
         return img, mask
